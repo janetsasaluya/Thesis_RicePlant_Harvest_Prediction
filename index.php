149498d4a3ff9605757b1049fa80feabce8bb2dd
janetsasaluya <jawjawsaluya@gmail.com>
2021-11-02 21:10:05 +0800

    Initial commit

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 			<!-- Header -->
 			<div>
 				<div>
-					<h3>NOTE: STRICTLY UPLOAD RICE PLANT IMAGES ONLY!!!</h3>
+					<h5>NOTE: STRICTLY UPLOAD RICE PLANT IMAGES ONLY!!!</h5>
 				</div>
 				<div>
 					<a class="mr-1" href="help.php">HOW TO IDENTIFY MONTH</a>
